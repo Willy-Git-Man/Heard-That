a44a80a1d800acd7f88c7d5389d640024b79c3c9
Willy-Git-Man <83353737+Willy-Git-Man@users.noreply.github.com>
2022-02-03 13:13:06 -0500

    fetch works to api

--- backend/app.js
+++ backend/app.js
@@ -39,7 +39,7 @@ app.use(
         httpOnly: true,
       },
     })
-    );
+   );
 
     app.use(routes); // Connect all the routes
 module.exports = app;
---
+++ backend/routes/api/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+router.post('/test', function(req, res) {
+  res.json({ requestBody: req.body });
+});
+
+router.get('/test', function(req, res) {
+  res.json("Will");
+});
+
+module.exports = router;
