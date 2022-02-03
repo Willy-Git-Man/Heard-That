f8054b53dbd435351e1a7f0362b298abf399b82f
Willy-Git-Man <83353737+Willy-Git-Man@users.noreply.github.com>
2022-02-03 15:13:43 -0500

    login validation working for username email and password

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -5,13 +5,32 @@ const asyncHandler = require('express-async-handler'); //The asyncHandler functi
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation'); //The check function from express-validator will be used with the handleValidationErrors to validate the body of a request.
+
+
 const router = express.Router();
+
+//The POST /api/session login route will expect the body of the request to have a key of credential with either the username or email of a user and a key of password with the password of the user.
+const validateLogin = [ // custom validateLogin that will check these keys and validate them:
+  // It checks to see whether or not req.body.credential and req.body.password are empty. If one of them is empty, then an error will be returned as the response.
+
+    check('credential')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .withMessage('Please provide a valid email or username.'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .withMessage('Please provide a password.'),
+    handleValidationErrors
+  ];
 
 
 // Log in
 //Next, add the POST /api/session route to the router using an asynchronous route handler. In the route handler, call the login static method from the User model. If there is a user returned from the login static method, then call setTokenCookie and return a JSON response with the user information. If there is no user returned from the login static method, then create a "Login failed" error and invoke the next error-handling middleware with it.
 router.post(
   '/',
+  validateLogin, //connect the POST /api/session route to the validateLogin middleware
   asyncHandler(async (req, res, next) => {
     const { credential, password } = req.body;
 
@@ -59,5 +78,7 @@ router.get(
     } else return res.json({});
   }
 );//You should see the current session user information if you have the token cookie. If you don't have a token cookie, you should see an empty object returned.
+
+
 
 module.exports = router
