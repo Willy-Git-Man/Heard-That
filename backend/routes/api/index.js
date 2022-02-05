f12473af91c49de7606cdabc3b476bf752dc887b
Willy-Git-Man <83353737+Willy-Git-Man@users.noreply.github.com>
2022-02-05 09:20:28 -0500

    get route for songs working on postman song foler set up about to write redux and song display component

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,11 +1,13 @@
 const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
+const songsRouter = require('./songs.js')
 
 router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
 
+router.use('/songs', songsRouter)
 // router.post('/test', (req, res) => {
 //   res.json({ requestBody: req.body });
 // });
---
+++ backend/routes/api/songs.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const { User, Songs } = require("../../db/models");
+const asyncHandler = require("express-async-handler");
+
+
+
+
+
+
+router.get("/",
+asyncHandler(async (req,res) => {
+  const songs = await Songs.findAll()
+
+  return res.json({
+    songs,
+  })
+})
+)
+
+
+
+
+module.exports = router;
