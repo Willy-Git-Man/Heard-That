7660116fd1cb9d342fa853a15fd8a04ea2c114df
Willy-Git-Man <83353737+Willy-Git-Man@users.noreply.github.com>
2022-02-05 15:15:08 -0500

    added action action creator and thunk for update song reducer not done no front end done, need to add user handling to every route so only user w user Id can update song

--- frontend/src/store/songs.js
+++ frontend/src/store/songs.js
@@ -2,6 +2,7 @@ import { csrfFetch } from "./csrf";
 
 const GET_ALL_SONGS = "songs/GET_ALL_SONGS";
 const ADD_SONG = "songs/ADD_SONG";
+const UPDATE_SONG ="songs/UPDATE_SONG"
 
 const getAllSongs = (allSongs) => ({
   type: GET_ALL_SONGS,
@@ -12,6 +13,11 @@ const addSong = (newSong) => ({
   type: ADD_SONG,
   payload: newSong,
 });
+
+const updateSong = (updatedSong) => ({
+  type: UPDATE_SONG,
+  payload: updatedSong
+})
 
 export const getAllSongsThunk = () => async (dispatch) => {
   const response = await csrfFetch("/api/songs");
@@ -35,3 +41,14 @@ export const addSongThunk = (newSong) => async (dispatch) => {
     return songRequest;
   }
 };
+
+export const updateSongThunk = (updatedSong) => async (dispatch) => {
+  const response = await csrfFetch(`/api/songs/${+updatedSong.id}`, {
+    method: 'PUT',
+    body: JSON.stringify(updatedSong)
+  })
+  if (response.ok) {
+    const updatedSongRequest = await response.json()
+    dispatch(updateSong(updatedSongRequest))
+  }
+}
