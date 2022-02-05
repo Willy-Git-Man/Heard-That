41b83760ce6ec588ec1748344b162922d8679ae5
Willy-Git-Man <83353737+Willy-Git-Man@users.noreply.github.com>
2022-02-05 14:46:42 -0500

    put route is working in console going to add put redux next still no front end

--- backend/routes/api/songs.js
+++ backend/routes/api/songs.js
@@ -3,32 +3,43 @@ const router = express.Router();
 const { User, Songs } = require("../../db/models");
 const asyncHandler = require("express-async-handler");
 
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    const songs = await Songs.findAll();
 
-
-
-
-
-router.get("/",
-asyncHandler(async (req,res) => {
-  const songs = await Songs.findAll()
-
-  return res.json({
-    songs,
+    return res.json({
+      songs,
+    });
   })
-})
-)
-
+);
 
 router.post(
-  "/",
+  '/',
   asyncHandler(async (req, res) => {
     const createSong = await Songs.create(req.body);
-    
+    // console.log('createSong:', createSong)
+
     return res.json({
       createSong,
     });
   })
 );
 
+
+router.put(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    const songChanges = req.body;
+
+    await Songs.update(songChanges, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    return res.json(songChanges);
+  })
+);
 
 module.exports = router;
