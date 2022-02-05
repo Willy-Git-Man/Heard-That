1d5d90297a509388b58cb9c3f985c87b3dd1ca30
Willy-Git-Man <83353737+Willy-Git-Man@users.noreply.github.com>
2022-02-05 14:00:14 -0500

    added action action creator and thunk for addSong, no reducer yet

--- frontend/src/store/songs.js
+++ frontend/src/store/songs.js
@@ -1,16 +1,20 @@
 import { csrfFetch } from "./csrf";
 
-const GET_ALL_SONGS = 'songs/GET_ALL_SONGS'
-
+const GET_ALL_SONGS = "songs/GET_ALL_SONGS";
+const ADD_SONG = "songs/ADD_SONG";
 
 const getAllSongs = (allSongs) => ({
   type: GET_ALL_SONGS,
-  payload: allSongs
-})
+  payload: allSongs,
+});
 
+const addSong = (newSong) => ({
+  type: ADD_SONG,
+  payload: newSong,
+});
 
-export const getAllEventsThunk = () => async (dispatch) => {
-  const response = await csrfFetch("/api/events");
+export const getAllSongsThunk = () => async (dispatch) => {
+  const response = await csrfFetch("/api/songs");
 
   if (response.ok) {
     const songs = await response.json();
@@ -19,4 +23,15 @@ export const getAllEventsThunk = () => async (dispatch) => {
   return response;
 };
 
-
+export const addSongThunk = (newSong) => async (dispatch) => {
+  const response = await csrfFetch("/api/songs", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(newSong),
+  });
+  if (response.ok) {
+    const songRequest = await response.json();
+    dispatch(addSong(songRequest));
+    return songRequest;
+  }
+};
