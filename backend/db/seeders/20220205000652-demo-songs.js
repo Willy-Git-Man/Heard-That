943b65b9f7490c2b8da2dbd02fcc48de19504927
Willy-Git-Man <83353737+Willy-Git-Man@users.noreply.github.com>
2022-02-06 11:31:29 -0500

    added labels and input fields and wrote the usestate hook for create song

--- backend/db/seeders/20220205000652-demo-songs.js
+++ backend/db/seeders/20220205000652-demo-songs.js
@@ -14,8 +14,8 @@ module.exports = {
     */
       return queryInterface.bulkInsert('Songs', [
         { userId: 1, albumId: 1, songName: 'Party in the USA',artistName: 'Miley Cyrus', songUrl: "www.howdoIfindsongurl.com", createdAt: new Date(), updatedAt: new Date(), imageUrl: "https://imageio.forbes.com/specials-images/imageserve/61d068989802259093ce8cd2/Tom-Ford-AW20-Show---Arrivals/960x0.jpg?fit=bounds&format=jpg&width=960"},
-        { userId: 1, albumId: 2, songName: 'Free Fallin',artistName: 'Tom Petty', songUrl: "www.howdoIfindsongurl2.com", createdAt: new Date(), updatedAt: new Date(), imageUrl: "https://imageio.forbes.com/specials-images/imageserve/61d068989802259093ce8cd2/Tom-Ford-AW20-Show---Arrivals/960x0.jpg?fit=bounds&format=jpg&width=960"},
-        { userId: 1, albumId: 1, songName: 'Feelin good again',artistName: 'Rober earl keen', songUrl: "www.howdoIfindsongurl3.com", createdAt: new Date(), updatedAt: new Date(), imageUrl: "https://imageio.forbes.com/specials-images/imageserve/61d068989802259093ce8cd2/Tom-Ford-AW20-Show---Arrivals/960x0.jpg?fit=bounds&format=jpg&width=960"},
+        { userId: 1, albumId: 2, songName: 'Free Fallin',artistName: 'Tom Petty', songUrl: "www.howdoIfindsongurl2.com", createdAt: new Date(), updatedAt: new Date(), imageUrl: "https://whythebeatlesarestillrelevant.files.wordpress.com/2020/04/free-fallin.jpg"},
+        { userId: 1, albumId: 1, songName: 'Feelin good again',artistName: 'Rober earl keen', songUrl: "www.howdoIfindsongurl3.com", createdAt: new Date(), updatedAt: new Date(), imageUrl: "https://s3.amazonaws.com/thisismyjam/i/0f3c140059756962b1df1064e0a4fa5d_395.jpg?1337407578"},
 
 
 
--- frontend/src/components/CreateSongModal/CreateSongFrom.js
+++ frontend/src/components/CreateSongModal/CreateSongFrom.js
@@ -1,9 +1,24 @@
+import { useState } from "react"
 import { useDispatch } from "react-redux"
 import { useHistory } from "react-router-dom"
 import { addSongThunk } from "../../store/songs"
 
 
 export default function CreateSongForm() {
+
+  const [songName, setSongName] = useState("Fake Song")
+  const [artistName, setArtistName] = useState("Fake Artist")
+  const [songUrl, setSongUrl] = useState("Fake song url")
+  const [imageUrl, setImageUrl] = useState("https://upload.wikimedia.org/wikipedia/en/1/1c/LightningBoltSkull.gif")
+  const [albumId, setAlumbId] = useState(1)
+
+  const newSongName = (e) => setSongName(e.target.value)
+  const newArtistName = (e) => setArtistName(e.target.value)
+  const newSongUrl = (e) => setSongUrl(e.target.value)
+  const newImageUrl = (e) =>  setImageUrl(e.target.value)
+  const newAlbumId = (e) => setAlumbId(e.target.value)
+
+
 const history = useHistory()
 const dispatch = useDispatch()
 
@@ -11,12 +26,12 @@ const handleSubmit = async (e) => {
   e.preventDefault()
 
   const newSongPayload = {
-    userId: 1,
-    albumId: 1,
-    songName: 'hardCodeSong',
-    artistName: 'harderCodedArtist',
-    songUrl: 'www.hardestCode.com',
-    imageUrl: 'https://imageio.forbes.com/specials-images/imageserve/61d068989802259093ce8cd2/Tom-Ford-AW20-Show---Arrivals/960x0.jpg?fit=bounds&format=jpg&width=960'
+    songName,
+    artistName,
+    songUrl,
+    imageUrl,
+    albumId,
+    userId: 1
   }
   dispatch(addSongThunk(newSongPayload))
   history.push('/Songs')
@@ -26,6 +41,21 @@ const handleSubmit = async (e) => {
     <>
     <div className="createNewSongDiv">
       <form className="createNewSongForm" onSubmit={handleSubmit} >
+
+        <label htmlFor="songNameLabel">Song Name: </label>
+        <input type="text" name="songName" value={songName} onChange={newSongName} required />
+
+        <label htmlFor="artistNameLabel">Artist Name: </label>
+        <input type="text" name="artistName" value={artistName} onChange={newArtistName} required />
+
+        <label htmlFor="songUrlLabel">Song Url: </label>
+        <input type="text" name="songUrl" value={songUrl} onChange={newSongUrl} required />
+
+        <label htmlFor="imageUrlLabel">Image Url: </label>
+        <input type="text" name="imageUrl" value={imageUrl} onChange={newImageUrl} required />
+
+        <label htmlFor="albumIdLabel">Ablum Id: </label>
+        <input type="text" name="albumId" value={albumId} onChange={newAlbumId} required />
 
         <button>Create Song</button>
       </form>
