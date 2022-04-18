dc3a2b29cb42f6ef8ff214b4bda773404c4c6602
Willy-Git-Man <83353737+Willy-Git-Man@users.noreply.github.com>
2022-04-18 13:32:24 -0400

    reset db

--- backend/db/seeders/20220209221533-demo-albums.js
+++ backend/db/seeders/20220209221533-demo-albums.js
@@ -9,8 +9,10 @@ module.exports = {
       Example:
       */
       return queryInterface.bulkInsert('Albums', [
-        {userId: 1, title: 'Misfit Songs', imageUrl:"https://cdn.shopify.com/s/files/1/0265/2585/9919/collections/COVER_MISFIT_TOYS_TPB_111019_print_1024x1024.jpg?v=1588732518", createdAt: new Date(), updatedAt: new Date(),},
-        {userId: 1, title: 'My Songs', imageUrl:"https://tonyaduncanellis.com/wp-content/uploads/2020/08/mixtape.jpg", createdAt: new Date(), updatedAt: new Date(),},
+        {userId: 2, title: 'Misfit Songs', imageUrl:"https://cdn.shopify.com/s/files/1/0265/2585/9919/collections/COVER_MISFIT_TOYS_TPB_111019_print_1024x1024.jpg?v=1588732518", createdAt: new Date(), updatedAt: new Date(),},
+        {userId: 2, title: 'My Playlist', imageUrl:"https://tonyaduncanellis.com/wp-content/uploads/2020/08/mixtape.jpg", createdAt: new Date(), updatedAt: new Date(),},
+        {userId: 2, title: 'Liked Songs', imageUrl:"https://cutewallpaper.org/24/heart-image-png/hd-white-heart-shape-silhouette-png-citypng.png", createdAt: new Date(), updatedAt: new Date(),},
+
         // {userId: 1, title: 'Summer Vibes', imageUrl:"https://i1.sndcdn.com/artworks-kpswMSYYQ4osELsK-jbOC2w-t500x500.jpg", createdAt: new Date(), updatedAt: new Date(),},
         //{userId: 1, title: 'Post Test', imageUrl:"https://i1.sndcdn.com/artworks-kpswMSYYQ4osELsK-jbOC2w-t500x500.jpg", createdAt: new Date(), updatedAt: new Date(),},
         //{userId: 1, title: 'Update Test', imageUrl:"https://i1.sndcdn.com/artworks-kpswMSYYQ4osELsK-jbOC2w-t500x500.jpg", createdAt: new Date(), updatedAt: new Date(),},
