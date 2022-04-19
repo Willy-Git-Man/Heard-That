32ee64ff9de0d1a5b8b55ca565dd28850edad239
Willy-Git-Man <83353737+Willy-Git-Man@users.noreply.github.com>
2022-04-19 11:04:18 -0400

    test

--- backend/app.js
+++ backend/app.js
@@ -24,11 +24,11 @@ if (!isProduction) {
 }
 
 // helmet helps set a variety of headers to better secure your app
-app.use(
-  helmet.crossOriginResourcePolicy({
-    policy: "cross-origin",
-  })
-);
+// app.use(
+//   helmet.crossOriginResourcePolicy({
+//     policy: "cross-origin",
+//   })
+// );
 
 app.use(helmet({
   contentSecurityPolicy: false
